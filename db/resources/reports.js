cfb6364357f86312a1c01116416c17bbae0e19b7
Gus Ireland <gus.ireland@rackspace.com>
2015-11-22 14:43:17 -0600

    Dumb bugfixes

--- db/resources/reports.js
+++ db/resources/reports.js
@@ -1,9 +1,11 @@
+const query = require('pg-query');
+
 module.exports = {
   list: function(callback) {
     // TODO
     callback(null);
   },
   get: function(id, callback) {
-    query('SELECT * FROM LOCATIONS WHERE LOCATION_ID = $1', [id], callback);
+    query('SELECT * FROM REPORTS WHERE LOCATION_ID = $1', [id], callback);
   }
 };
