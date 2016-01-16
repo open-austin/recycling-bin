569840eb5462eeebff7064ce6936b7cfbdcadbdc
Preston Pham <ppham@us.ibm.com>
2016-01-16 12:51:35 -0600

    Add report endpoint

--- db/resources/reports.js
+++ db/resources/reports.js
@@ -7,5 +7,11 @@ module.exports = {
   },
   get: function(id, callback) {
     query('SELECT * FROM REPORTS WHERE LOCATION_ID = $1', [id], callback);
+  },
+  insert: function(locationId, report, callback) {
+    query.first('INSERT INTO REPORTS (location_id, report) VALUES ($1, $2) RETURNING *',
+     [locationId, report],
+     callback
+   );
   }
 };
