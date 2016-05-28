5fd5f039ffef071e9cb06d2e95fd31ccc3c55826
Gus Ireland <gus.ireland@rackspace.com>
2016-05-28 12:41:33 -0500

    more console statements

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var connectionStr = process.env.RDS_USERNAME ? 'postgres://' + process.env.RDS_USERNAME + ':' + process.env.RDS_PASSWORD + '@' + process.env.RDS_HOSTNAME + ':' + process.env.RDS_PORT + '/' + process.env.RDS_DB_NAME : 'postgres://localhost:5432/recycling';
-
+console.log(connectionStr);
 module.exports = {
   db: {
 	  connectionStr: connectionStr
