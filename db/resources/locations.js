dbf7676939d86e86ed47a4ad7f07a5c0f598282a
Gus Ireland <gus.ireland@rackspace.com>
2016-05-28 12:04:25 -0500

    changed case of tables

--- db/resources/locations.js
+++ db/resources/locations.js
@@ -3,14 +3,14 @@ const query = require('pg-query');
 module.exports = {
   list: function(callback) {
     query(`SELECT *,
-      (select COUNT(*) from reports where reports.location_id = locations.id) as reports_count FROM LOCATIONS`, callback);
+      (select COUNT(*) from reports where reports.location_id = locations.id) as reports_count FROM locations`, callback);
   },
 
   get: function(id, callback) {
     query.first(`SELECT *,
       (select json_agg(r)
         from (select id, report from reports where reports.location_id = locations.id) as r) as reports
-      FROM LOCATIONS WHERE ID = $1`, [id], callback);
+      FROM locations WHERE ID = $1`, [id], callback);
   },
 
   search: function(address, callback) {
@@ -22,7 +22,7 @@ module.exports = {
     var lat = data.payload.coordinates[0];
     var lng = data.payload.coordinates[1];
     var address = data.payload.address;
-    query('INSERT INTO LOCATIONS (name, coordinates, address) VALUES ($1, point($2, $3), $4) RETURNING *',
+    query('INSERT INTO locations (name, coordinates, address) VALUES ($1, point($2, $3), $4) RETURNING *',
      [name, lat, lng, address],
      function(err, rows, result) {
        return callback(err, err ? null : rows[0]);
@@ -32,7 +32,7 @@ module.exports = {
 
   update: function(id, data, callback) {
     //TODO how should reports be stored?
-    query('INSERT INTO REPORTS (location_id, report) VALUES ($1, $2)',[id, data.payload.report], callback);
+    query('INSERT INTO reports (location_id, report) VALUES ($1, $2)',[id, data.payload.report], callback);
     //callback(null, data);
   }
 };
